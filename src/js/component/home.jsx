c4a8839ecbaaa21e069125d8a23027dfcff0a104
Hector Ector <163536279+AmrHector@users.noreply.github.com>
2024-06-17 21:57:39 +0000

    Finished Todo List

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,16 @@
-import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React from "react";
+import { Todo } from "./Todo";
+import { TodoWrapper } from "./TodoWrapper";
+import { TodoForm } from "./TodoForm";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="App">
+			<TodoWrapper />
 		</div>
 	);
 };
-
 export default Home;
+//create your first component
